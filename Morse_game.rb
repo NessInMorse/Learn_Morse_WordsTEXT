81edcd5fa20409878e6eb9389d42aca7657f7db2
NessInMorse <56824499+NessInMorse@users.noreply.github.com>
2020-07-23 03:03:31 +0200

    Create Morse_game.rb

---
+++ Morse_game.rb
@@ -0,0 +1,75 @@
+require "path/to/alle_woorden.rb"
+
+include Words
+
+
+loadtime_a=0
+loadtime_b=0
+loadtime_a=Time.now
+length=Words.Dictionary.length
+random=rand(length)
+dictionary=Words.Dictionary
+morsedictionary=Words.MorseDictionary
+response=""
+ask=morsedictionary[random]
+answer=dictionary[random]
+loadtime_b=Time.now
+yes_no=""
+begintime=0
+endtime=0
+
+#can also be done by hand by adding a "Races.csv"-file with: "Racenumber,Speed,Woord,Characters,Time" in the same folder and removing line 22-26 of this program
+unless File.file?("D:/Programming/Ruby Folder/Learning_Morse_Code/Races.csv")
+        File.open("D:/Programming/Ruby Folder/Learning_Morse_Code/Races.csv","w") do |data|
+            data.puts "Racenumber,Speed,Woord,Characters,Time"
+        end
+end
+
+
+
+racenumber=0
+
+File.open("D:/Programming/Ruby Folder/Learning_Morse_Code/Races.csv","r") do |data|
+        for line in data.readlines()
+        racenumber+=1
+        end
+end
+
+while response !="n"
+  sleep(0.5)
+  puts "The word you have to translate is:"
+  puts ask[1..ask.length] 
+  begintime=Time.now
+
+    while answer.downcase!=response.downcase
+      response=gets.chomp()
+
+      if response=="n"
+        break;
+      end
+    end
+    
+    endtime=Time.now
+
+    File.open("D:/Programming/Ruby Folder/Learning_Morse_Code/Races.csv","a") do |data|
+      data.puts "#{racenumber},#{(answer.length.to_f/5)/( (endtime.to_f-begintime.to_f)/60 ).round(3)},#{answer},#{answer.length},#{begintime}"
+      racenumber+=1
+    end
+
+
+    puts "Het duurde #{(endtime.to_f)-(begintime.to_f)} seconds"
+    puts "Dat is #{answer.length/(endtime.to_f-begintime.to_f)} characters per second"
+    puts "De dat is #{(answer.length.to_f/5)/( (endtime.to_f-begintime.to_f)/60 )} WPM"
+    puts "De tijd om het bestand te laden was: #{loadtime_b.to_f-loadtime_a.to_f} seconds"
+    puts "want to try again"
+    
+    yes_no=gets.chomp()
+    if yes_no=="y" || yes_no="yes" || yes_no="ye" || yes_no==""
+        loadtime_a=Time.now
+        random=rand(length)
+        ask=morsedictionary[random]
+        answer=dictionary[random]
+        loadtime_b=Time.now
+        else response="n"
+    end
+end

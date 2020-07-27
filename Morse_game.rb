1fe43ec91b02c66de9007c343e0e0fc0e0d68483
NessInMorse <56824499+NessInMorse@users.noreply.github.com>
2020-07-28 01:54:23 +0200

    Update Morse_game.rb

--- Morse_game.rb
+++ Morse_game.rb
@@ -1,6 +1,47 @@
 require "path/to/alle_woorden.rb"
 
 include Words
+require 'curses'
+
+input=""
+rawinput=""
+rawtime=[]
+c=0
+time=   [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+count=  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+line_c=0
+inter_c=0
+time0=0
+time1=0
+lettertimes=""
+
+File.open("path/to/Letter_graph.txt","r+") do |data|
+        for line in data.readlines()
+              
+                if line_c==0
+                        line.tr("[]","")
+                        if line.index(",")!=nil
+                                line.split(",").each do |z|
+                                        count[inter_c]=z.to_i
+                                        inter_c+=1
+                                end
+                        end
+                end
+                
+                if line_c==1
+                        line.tr("[]","")
+                        if line.index(",")!=nil
+                                line.split(",").each do |z|
+                                        time[inter_c]=z.to_i
+                                        inter_c+=1
+                               end
+                        end
+                end
+
+                line_c+=1
+                inter_c=0
+        end
+end
 
 
 loadtime_a=0
@@ -18,73 +59,135 @@ yes_no=""
 begintime=0
 endtime=0
 
-#can also be done by hand by adding a "Races.csv"-file with: "Racenumber,Speed,Woord,Characters,Time,Morselength" in the same folder and removing line 22-26 of this program
-unless File.file?("D:/Programming/Ruby Folder/Learning_Morse_Code/Races.csv")
-        File.open("D:/Programming/Ruby Folder/Learning_Morse_Code/Races.csv","w") do |data|
-            data.puts "Racenumber,Speed,Woord,Characters,Time,Morselength"
-        end
-end
-
-
+input=0
 
 racenumber=0
-
-File.open("D:/Programming/Ruby Folder/Learning_Morse_Code/Races.csv","r") do |data|
+morselength=0
+File.open("path/to/Races.csv","r") do |data|
         for line in data.readlines()
-        racenumber+=1
+                racenumber+=1
         end
 end
 
-while response !="n"
-        
-      puts "race starts in 2 seconds"
-      sleep(1)
-      puts "race starts in 1 second"
-      sleep(1)
 
-  puts "The word you have to translate is:"
-  puts ask[1..ask.length] 
-  begintime=Time.now
-
-    while answer.downcase!=response.downcase
-      response=gets.chomp()
-
-      if response=="n"
-        break;
-      end
-    end
-    
-    endtime=Time.now
-
-    File.open("D:/Programming/Ruby Folder/Learning_Morse_Code/Races.csv","a") do |data|
-      data.puts "#{racenumber},#{(answer.length.to_f/5)/( (endtime.to_f-begintime.to_f)/60 ).round(3)},#{answer},#{answer.length},#{begintime},#{ask[1..ask.length].length}"
-      racenumber+=1
-    end
+puts "Are you ready?"
+pineapple=gets.chomp()
 
 
-    puts "It took #{(endtime.to_f)-(begintime.to_f)} seconds"
-    puts "That's #{answer.length/(endtime.to_f-begintime.to_f)} characters per second"
-    puts "That's #{(answer.length.to_f/5)/( (endtime.to_f-begintime.to_f)/60 )} WPM"
-    puts "Loading time: #{loadtime_b.to_f-loadtime_a.to_f} seconds"
-    
-    if racenumber%100==0
-        puts "You have raced #{racenumber} times!"
-    end
-        
-        
-    puts "want to try again?"
-    
-        
-        
-        
-        
-    yes_no=gets.chomp()
-    if yes_no=="y" || yes_no="yes" || yes_no="ye" || yes_no==""
-        loadtime_a=Time.now
-        random=rand(length)
-        ask=morsedictionary[random]
-        answer=dictionary[random]
-        loadtime_b=Time.now
-        else response="n"
-    end
+def GoSleep(time)
+        sleep(time)
 end
+
+
+
+
+while response !="n"
+        puts "\nrace starts in 2 seconds"
+        sleep(1)
+        puts "race starts in 1 second"
+        sleep(1)
+        
+        
+        
+        Curses.init_screen
+        begin
+                Curses.addstr("The word you have to translate is: \n")
+                Curses.addstr("#{ask[1..ask.length]} \n")
+                begintime=Time.now
+
+                while rawinput.index(answer.downcase)==nil
+
+                        time0=(Time.now.to_f*1000)
+
+                        input = Curses.getch
+
+                        unless input.ord<97 || input.ord>122
+                                rawinput+=input
+                                time1=(Time.now.to_f*1000)
+                                rawtime<<(time1-time0)
+                        end
+
+                        if input.ord==8 && rawinput.length>0
+                                rawinput[rawinput.length-1]=""
+                        end
+
+                        if response=="n"
+                                break;
+                        end
+                end
+
+
+
+                endtime=Time.now
+                unless rawinput.length != answer.length
+                        while c<answer.length
+                                time[rawinput[rawinput.length-1-c].ord-96] = (time[rawinput[rawinput.length-1-c].ord-96]*count[rawinput[rawinput.length-1-c].ord-96] + rawtime[rawtime.length-1-c])/(count[rawinput[rawinput.length-1-c].ord-96]+1)
+                                count[rawinput[rawinput.length-1-c].ord-96] +=1
+                                c+=1
+                        end
+                end
+                c=0
+
+                line_c=0
+                File.open("path/to/Letter_graph.txt","w") do |data|
+                        data.puts count.to_s
+                        data.puts time.to_s
+                end
+
+
+
+                File.open("path/to/Races.csv","a") do |data|
+                        data.puts "#{racenumber},#{(answer.length.to_f/5)/( (endtime.to_f-begintime.to_f)/60 ).round(3)},#{answer},#{answer.length},#{begintime},#{ask[1..ask.length].length}"
+                        racenumber+=1
+                end
+
+                Curses.addstr("\nHet duurde #{(endtime.to_f)-(begintime.to_f)} seconden \n")
+                Curses.addstr("Dat is #{answer.length/(endtime.to_f-begintime.to_f)} tekens per seconde \n") 
+                Curses.addstr("De dat is #{(answer.length.to_f/5)/( (endtime.to_f-begintime.to_f)/60 )} WPM \n")
+                Curses.addstr("De tijd om het bestand te laden was: #{loadtime_b.to_f-loadtime_a.to_f} seconden \n")
+
+                #only in cases where the final string is exactly the same as the requested answer
+                if answer.length == rawinput.length
+                        while c<answer.length
+                                lettertimes+="[#{answer[c]}|#{rawtime[c]}ms] "
+                                c+=1
+                        end
+                        Curses.addstr("#{lettertimes} \n")
+                        c=0
+                        lettertimes=""
+                end
+                
+                
+
+                if racenumber%100==1
+                        Curses.addstr("You have raced #{racenumber-1} times! \n") 
+                end
+
+                Curses.addstr("wanna race again? \n")
+
+                while yes_no.index("y")==nil
+                        input=Curses.getch
+                        if input.ord==13 || input.ord==10
+                                yes_no+="y"
+                        end
+                        unless input.ord<97 || input.ord>122 || input.ord==13 || input.ord==10 || input.ord==8
+                                yes_no+=input.downcase
+                        end
+
+                        if yes_no.index("y")!=nil
+                                loadtime_a=Time.now
+                                random=rand(length)
+                                ask=morsedictionary[random]
+                                answer=dictionary[random]
+                                loadtime_b=Time.now
+                                Curses.clear
+                        end
+                end
+                yes_no=""
+                rawinput=""
+                rawtime=[]
+                ensure
+                Curses.close_screen
+        end
+end
+puts length
